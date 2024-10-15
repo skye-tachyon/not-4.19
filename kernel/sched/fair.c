c7375cc7df50cf4e74046cc511e5ccb0690d6551
EmanuelCN <emanuelghub@gmail.com>
2024-10-15 19:46:38 +0300

    Revert "ANDROID: sched: Prevent unnecessary active balance of single task in sched group"
    This reverts commit 65b8ddef4ef06f78b29207ef5ec508fb58ad807f.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8402,7 +8402,6 @@ struct lb_env {
 	int			new_dst_cpu;
 	enum cpu_idle_type	idle;
 	long			imbalance;
-	unsigned int		src_grp_nr_running;
 	/* The set of CPUs under consideration for load-balancing */
 	struct cpumask		*cpus;
 
@@ -9807,8 +9806,6 @@ next_group:
 	if (env->sd->flags & SD_NUMA)
 		env->fbq_type = fbq_classify_group(&sds->busiest_stat);
 
-	env->src_grp_nr_running = sds->busiest_stat.sum_nr_running;
-
 	if (!env->sd->parent) {
 		struct root_domain *rd = env->dst_rq->rd;
 
@@ -10593,10 +10590,8 @@ no_move:
 		 * frequent, pollute the failure counter causing
 		 * excessive cache_hot migrations and active balances.
 		 */
-		if (idle != CPU_NEWLY_IDLE) {
-			if (env.src_grp_nr_running > 1)
-				sd->nr_balance_failed++;
-		}
+		if (idle != CPU_NEWLY_IDLE)
+			sd->nr_balance_failed++;
 
 		if (need_active_balance(&env)) {
 			unsigned long flags;
