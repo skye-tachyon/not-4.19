d523b48a3af70e5793fafb553af7c8529cf5fbff
Atakan <ata.kaner03@gmail.com>
2025-10-26 11:26:18 +0300

    input: touchscreen: zt7650: Do not probe if touchscreen is not attached
    We don't want continue to probe it when device has a STM touchscreen.
    
    Change-Id: Ib78da7f425d3578c9f239af32c9b545b72915482

--- drivers/input/touchscreen/zinitix/zt7650/zinitix_ts.h
+++ drivers/input/touchscreen/zinitix/zt7650/zinitix_ts.h
@@ -52,6 +52,10 @@
 
 #include "../../../sec_input/sec_input.h"
 
+#ifdef CONFIG_DISPLAY_SAMSUNG
+extern int get_lcd_attached(char *mode);
+#endif
+
 #define ZINITIX_REG_ADDR_LENGTH	2
 
 #define TSP_TYPE_BUILTIN_FW			0
